dfb7eede270a25132271b0511213ce2eaf9d6d20
zblfendou <zhangbinleifendou@163.com>
2018-12-21 13:47:32 +0800

    优化

--- services/managerService/src/main/java/zbl/fly/daos/ManagerDao.java
+++ services/managerService/src/main/java/zbl/fly/daos/ManagerDao.java
@@ -3,5 +3,5 @@ package zbl.fly.daos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zbl.fly.models.Manager;
 
-public interface UserDao extends JpaRepository<Manager, Long> {
+public interface ManagerDao extends JpaRepository<Manager, Long> {
 }
--- services/managerService/src/main/java/zbl/fly/service/ManagerServiceImpl.java
+++ services/managerService/src/main/java/zbl/fly/service/ManagerServiceImpl.java
@@ -5,7 +5,7 @@ import zbl.fly.api.remote.ManagerService;
 import javax.inject.Named;
 
 @Named("managerService")
-public class UserServiceImpl implements ManagerService {
+public class ManagerServiceImpl implements ManagerService {
 
     @Override
     public void test() {
