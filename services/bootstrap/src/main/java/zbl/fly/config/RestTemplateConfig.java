3320f9a4f42484c0a8e6fa5b463691963398cb0c
zblfendou <zhangbinleifendou@163.com>
2019-01-25 17:00:09 +0800

    restTemplate 支持https

---
+++ services/bootstrap/src/main/java/zbl/fly/config/RestTemplateConfig.java
@@ -0,0 +1,24 @@
+package zbl.fly.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(SimpleClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public SimpleClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        //支持https
+        SkipSslVerificationHttpRequestFactory factory = new SkipSslVerificationHttpRequestFactory();
+        factory.setConnectTimeout(15000);
+        factory.setReadTimeout(5000);
+        return factory;
+    }
+}
